1a553ca6ad6866afcdf16f9b1789a0e96b7d74eb
tzuchianti <tzuchianti@gmail.com>
2021-04-23 17:48:03 +0800

    format setting

--- catalog/views.py
+++ catalog/views.py
@@ -72,13 +72,6 @@ def venues(request):
     #return render(request, 'index.html', {'venues':venues, 'current_year':current_year,})
 
 
-# class VenueListView(generic.ListView):
-#     model = Venue
-#     paginate_by = 20
-#     #context_object_name = 'my_book_list'   # your own name for the list as a template variable
-#     #queryset = Book.objects.filter(title__icontains='Game')[:4] # Get 5 books containing the title war
-#     template_name = 'venues/venue_list.html'  # Specify your own template name/location
-    
 
 class VenueDetailView(generic.DetailView):
     model = Venue
@@ -93,7 +86,7 @@ class VenueInstanceListView(generic.ListView):
     template_name = 'VenueInstances/venueinstance_list.html' 
     
     
-# Forms setting
+# VenueForm setting
 def  add_venue(request):
     submitted = False
     if request.method == "POST" :
@@ -133,18 +126,7 @@ def venuedelete(request, id):
     return redirect('venues')
   
 
-# def delete(request,id=None):  #刪除資料
-# 	if id!=None:
-# 		if request.method == "POST":  #如果是以POST方式才處理
-# 			id=request.POST['venue_id'] #取得表單輸入的編號
-# 		try:
-# 			unit = Venue.objects.get(id=id)  
-# 			unit.delete()
-# 			return redirect('/index/')
-# 		except:
-# 			message = "讀取錯誤!"			
-# 	return render(request, "venue_delete.html", locals())
-
+#venueinstanceform  setting
 def  add_venueinstance(request):
     submitted = False
     if request.method == "POST" :
